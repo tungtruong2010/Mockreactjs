c10483d2516c406c00d6b949a9c8469efcc1ddb1
tungtruong2010 <59758249+tungtruong2010@users.noreply.github.com>
2022-06-07 00:10:33 +0700

    Upload Mock Reactjs

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header';
+import Table from './components/Table';
+import {BrowserRouter, Routes, Route, Navigate, useParams } from 'react-router-dom';
+import Home from './pages/Home';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+      <Route
+      path='*'
+        element={<Navigate to="/respo1" replace />}
+      />
+        {/* <Route path='/home' element = {<Home/>}></Route> */}
+        <Route path='/respo1' element={<Home />} />
+        <Route path='/respo2' element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
